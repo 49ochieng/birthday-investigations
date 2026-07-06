6226b8b2342ce09a8cf7ed31cdac9dfea1c4cb4b
Edgar ochieng <edgarmcochieng@gmail.com>
2026-07-06 16:29:00 -0500

    feat: wire up Sammy's page route

---
+++ src/routes/SammyPage.tsx
@@ -0,0 +1,6 @@
+import CaseFileLayout from "../components/CaseFileLayout";
+import sammyData from "../data/sammy";
+
+export default function SammyPage() {
+  return <CaseFileLayout data={sammyData} />;
+}
