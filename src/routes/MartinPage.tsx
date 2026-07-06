080cf28455eca05ac46adf1c8326e8c9a693830b
Edgar ochieng <edgarmcochieng@gmail.com>
2026-07-06 16:58:00 -0500

    feat: wire up Martin's page route

---
+++ src/routes/MartinPage.tsx
@@ -0,0 +1,6 @@
+import CaseFileLayout from "../components/CaseFileLayout";
+import martinData from "../data/martin";
+
+export default function MartinPage() {
+  return <CaseFileLayout data={martinData} />;
+}
